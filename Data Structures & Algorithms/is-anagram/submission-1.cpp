dc2e919cd9d292b41328a9348bc8d53dcd0a78e8
Manvir-coderbull <113898698+Manvir-coderbull@users.noreply.github.com>
2026-07-21 14:42:03 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        if(s.length() != t.length())
+        return false;
+
+        vector<int> v(26,0);
+        for(int i=0;i<s.length();i++)
+        {
+            v[s[i]-'a']++;
+            v[t[i]-'a']--;
+        }
+        for(int val: v)
+        {
+            if(val!=0)
+            return false;
+        }
+        return true;
+        
+    }
+};
